e575515285a41669ddbf5a4f077178e1cf69c8ba
Your Name <362293033@qq.com>
2017-09-02 14:21:49 +0800

    添加了gulp文件

---
+++ gulp/code/gulpfile.js
@@ -0,0 +1,24 @@
+
+
+
+
+var gulp = require('gulp');
+var babel = require('gulp-babel'); //es6转es5
+var uglify = require('gulp-uglify'); //js压缩插件
+
+
+
+
+
+
+
+
+
+//压缩js
+gulp.task('jsTask2', function(){
+	 gulp.src('src/js/*.js')
+	.pipe(babel({"presets": ["es2015"]})) //es6转es5
+	.pipe(uglify()) //js压缩
+	.pipe(gulp.dest('dest/js'));
+});
+gulp.task('default', ['jsTask2']);
